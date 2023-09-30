c09a3ce14aac0c62ac4674ce6d61fd5a41325abe
iqbalyarkhan <iqbalkhan@Iqbals-MBP.attlocal.net>
2023-09-30 01:23:04 -0500

    cleanup and typescript post

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,6 +44,18 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: `gatsby-remark-table-of-contents`,
+            options: {
+              exclude: "Table of Contents",
+              tight: false,
+              ordered: false,
+              fromHeading: 1,
+              toHeading: 6,
+              className: "table-of-contents"
+            },
+          },
+          `gatsby-remark-autolink-headers`,
+          {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 630,
